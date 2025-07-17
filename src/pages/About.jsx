3b07e34b719212bd32c7f07cfa4b938dc63605f0
Kiruthikan29 <kiruthikank29@gmail.com>
2025-07-17 21:43:28 +0530

    Update2 About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ const About = () => (
   <section className="about1">
     <h2 className="about2">About Me</h2>
     <p><strong>Education:</strong> BSc in Computer Engineering Undergraduate</p>
-    <p><strong>Interests:</strong> Web development, Coding in JAVA and AI</p>
+    <p><strong>Interests:</strong> Web development(FullStack), Coding in JAVA and AI</p>
     <p className="about3">
       I’m a passionate student with interest in software development,
       always eager to learn new technologies and build projects.
